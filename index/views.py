7541e8cfccf01289b7cf3f71346c89c308cfb4f5
jinshenzou <jinshenzou0710@outlook.com>
2019-07-26 23:05:30 +0800

    2305

--- index/views.py
+++ index/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import csv
+from .models import Product
 # Create your views here.
 def index(request):
-    return render(request,'index.html',context={'title':'首页'},status=500)
+    type_list = Product.objects.values('type').distinct()
+    name_list = Product.objects.values('name','type')
+    context = {
+        'title':'首页',
+        'type_list':type_list,
+        'name_list':name_list,
+    }
+    return render(request,'index.html',context=context,status=200)
 
 def mydate(request, year, month, day):
     return HttpResponse(str(year) + '/' + str(month) + '/' + str(day))
