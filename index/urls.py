a0b7d176d487ecc72700ed425942ac29ad1223b5
jinshenzou <jinshenzou0710@outlook.com>
2019-07-29 11:54:25 +0800

    11

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,re_path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
     #path('<year>/<int:month>/<slug:day>',views.mydate),
     re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html', views.mydate),
     # 参数为字典的URL
@@ -10,6 +10,6 @@ urlpatterns = [
     re_path('(?P<year>[0-9]{4}).html', views.myyear, name='myyear'),
     path('download.html', views.download),
     path('login.html', views.login),
-    path('index/', views.ProductList.as_view()),
+    path('index/', views.ProductList.as_view),
     path('test/',views.test)
 ]
